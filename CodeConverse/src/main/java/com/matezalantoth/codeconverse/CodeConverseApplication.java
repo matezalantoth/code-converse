de9316975c861d9e8054836e6be2cbc1be3d15f9
Mate Toth <mztbusinessenquiries@gmail.com>
2024-11-28 15:30:06 +0000

    initial commit

---
+++ CodeConverse/src/main/java/com/matezalantoth/codeconverse/CodeConverseApplication.java
@@ -0,0 +1,13 @@
+package com.matezalantoth.codeconverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeConverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeConverseApplication.class, args);
+    }
+
+}
---
+++ CodeConverse/src/test/java/com/matezalantoth/codeconverse/CodeConverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matezalantoth.codeconverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeConverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
