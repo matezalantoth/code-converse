1548b5eb1b68418c1eab1359fc09161f6aa35c89
Mate Toth <mztbusinessenquiries@gmail.com>
2024-12-05 11:43:20 +0000

    login and docker compose done

--- backend/src/main/java/com/matezalantoth/codeconverse/security/WebSecurityConfig.java
+++ backend/src/main/java/com/matezalantoth/codeconverse/security/WebSecurityConfig.java
@@ -19,6 +19,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+
+import java.util.Arrays;
 
 @EnableMethodSecurity
 @Configuration
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {ProfileGuardService} from "./services/profile-guard/profile-guard.service";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent, canActivate: [ProfileGuardService]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
